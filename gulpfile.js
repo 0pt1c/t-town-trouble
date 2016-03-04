29fcb72d3ffa63aeada073442bfca096d627a392
Chris Bryant <chris@urbandiscourse.com>
2016-03-03 23:42:55 -0600

    .ejs templates, new deps, passport

--- gulpfile.js
+++ gulpfile.js
@@ -33,7 +33,7 @@ gulp.task('inject', function() {
     ignorePath: '../../public'
   };
 
-  return gulp.src(['./src/views/*.jade', './src/views/*/*.html'])
+  return gulp.src(['./src/views/*.ejs', './src/views/*/*.html'])
     .pipe(wiredep(options))
     .pipe(inject(injectSrc, injectOptions))
     .pipe(gulp.dest('./src/views'));
