4a8b61a6abcf1ab16c67f19a9d9fc79083edcfb4
Chris Bryant <chris@urbandiscourse.com>
2016-03-12 01:04:32 -0600

    set bodyParser extended: false

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ var authRouter = require('./src/routes/authRoutes')(nav);
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('views', './src/views');
 
